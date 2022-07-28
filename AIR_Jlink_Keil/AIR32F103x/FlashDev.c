fd8eedcbc43a6a40ea0470d2426fd501967cc9f4
guoliang <15863338830@163.com>
2022-07-28 15:55:59 +0800

    add:pack新增RCT6和VET6

--- AIR_Jlink_Keil/AIR32F103x/FlashDev.c
+++ AIR_Jlink_Keil/AIR32F103x/FlashDev.c
@@ -61,4 +61,25 @@ struct FlashDevice const FlashDevice  =  {
    SECTOR_END
 };
 #endif
+#ifdef Air32F103VET6
+struct FlashDevice const FlashDevice  =  {
+   FLASH_DRV_VERS,             // Driver Version, do not modify!
+   "Air32F103VET6",// Device Name (512kB/384kB/256kB)
+   ONCHIP,                     // Device Type
+   0x08000000,                 // Device Start Address
+   0x00080000,                 // Device Size in Bytes (512kB)
+   1024,                       // Programming Page Size
+   0,                          // Reserved, must be 0
+   0xFF,                       // Initial Content of Erased Memory
+   100,                        // Program Page Timeout 100 mSec
+   500,                        // Erase Sector Timeout 500 mSec
+
+// Specify Size and Address of Sectors
+   0x0800, 0x000000,           // Sector Size 2kB (256 Sectors)
+   SECTOR_END
+};
+#endif
+
+
+
 
