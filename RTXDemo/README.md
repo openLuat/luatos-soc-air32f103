af674688cbc38a43a6c26a7833372c061fe80a48
chain001 <15863338830@163.com>
2022-12-02 17:31:33 +0800

    add:使用RTE配置的USB转串口示例

---
+++ RTXDemo/README.md
@@ -0,0 +1,4 @@
+# RTX相关demo
+
+使用本目录的demo前，必须先安装AIR_Jlink_Keil目录下的pack包，并升级到最新版
+
---
+++ RTXDemo/USB/USB_CDC_UART/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,41 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'USB_CDC_UART' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "air32f10x.h"
+
+/* ARM::CMSIS:RTOS2:Keil RTX5:Library:5.5.4 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+/* Keil.MDK-Plus::USB:CORE:Release:6.15.0 */
+#define RTE_USB_Core                    /* USB Core */
+          #define RTE_USB_Core_Release            /* USB Core Release Version */
+/* Keil.MDK-Plus::USB:Device:6.15.0 */
+#define RTE_USB_Device_0                /* USB Device 0 */
+
+/* Keil.MDK-Plus::USB:Device:CDC:6.15.0 */
+#define RTE_USB_Device_CDC_0            /* USB Device CDC instance 0 */
+
+/* Keil::CMSIS Driver:USART:1.0 */
+#define RTE_Drivers_USART1              /* Driver USART1 */
+        #define RTE_Drivers_USART2              /* Driver USART2 */
+        #define RTE_Drivers_USART3              /* Driver USART3 */
+        #define RTE_Drivers_USART4              /* Driver UART4  */
+        #define RTE_Drivers_USART5              /* Driver UART5  */
+/* Keil::CMSIS Driver:USB Device:1.0 */
+#define RTE_Drivers_USBD0               /* Driver USBD0 */
+
+
+#endif /* RTE_COMPONENTS_H */
